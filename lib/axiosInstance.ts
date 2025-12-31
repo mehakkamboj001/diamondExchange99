be649d1719a34b90f48731500fe5bd6f7479decd
Ravi <ravi@antino.com>
2025-12-31 12:31:43 +0530

    change url

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/v1',
+    baseURL: typeof window !== 'undefined' ? '/api/v1' : 'http://localhost:3000/api/v1',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
